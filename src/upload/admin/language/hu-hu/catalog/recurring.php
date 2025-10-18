d7444718c54ed3399c7e690678b37549b4676279
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-10-18 15:40:45 +0200

    translation update

--- src/upload/admin/language/hu-hu/catalog/recurring.php
+++ src/upload/admin/language/hu-hu/catalog/recurring.php
@@ -5,7 +5,7 @@ $_['heading_title']         = 'Előfizetések';
 // Text
 $_['text_success']          = 'Siker: Az előfizetési terv sikeresen módosítva lett!';
 $_['text_list']             = 'Előfizetési profil lista';
-$_['text_add']              = 'Előfizetés hzzáadása';
+$_['text_add']              = 'Előfizetés hozzáadása';
 $_['text_edit']             = 'Előfizetés szerkesztése';
 $_['text_day']              = 'Nap';
 $_['text_week']             = 'Hét';
@@ -14,14 +14,14 @@ $_['text_month']            = 'Hónap';
 $_['text_year']             = 'Év';
 $_['text_recurring']        = '<p><i class="fa fa-info-circle"></i> Az ismétlődő összegek a gyakoriság és ciklusok alapján kerülnek kiszámításra.</p><p>Például, ha a gyakoriság "hét" és a ciklus "2", akkor a felhasználót kéthetente számlázzák.</p><p>A futamidő a fizetések számát jelenti; állítsa ezt 0-ra, ha azt szeretné, hogy a fizetések addig folytatódjanak, amíg azokat le nem mondják.</p>';
 $_['text_profile']          = 'Előfizetési terv';
-$_['text_trial']            = 'Trial Profile';
+$_['text_trial']            = 'Próbaidőszaki profil';
 
 // Entry
-$_['entry_name']            = 'Előfizetési profil név';
+$_['entry_name']            = 'Előfizetési profil neve';
 $_['entry_price']           = 'Ár';
 $_['entry_duration']        = 'Időtartam';
 $_['entry_cycle']           = 'Ciklus';
-$_['entry_frequency']       = 'Frequency';
+$_['entry_frequency']       = 'Próbaidőszak fizetési gyakorisága';
 $_['entry_trial_price']     = 'Próbaidőszak ár';
 $_['entry_trial_duration']  = 'Próbaidőszak időtartama';
 $_['entry_trial_status']    = 'Próbaidőszak állapot';
@@ -31,7 +31,7 @@ $_['entry_status']          = 'Állapot';
 $_['entry_sort_order']      = 'Sorrend';
 
 // Column
-$_['column_name']           = 'Előfizetési profil név';
+$_['column_name']           = 'Előfizetési profil neve';
 $_['column_sort_order']     = 'Sorrend';
 $_['column_action']         = 'Művelet';
 
